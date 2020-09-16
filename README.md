9ea60fc3925e2e2eee52b310f2a4c26dd41b012c
Mike Conrad <mike@enxo.co>
2020-09-16 00:47:29 -0400

    Initial Commit, fully working code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zen Mode
+
+** This is a very minimal example Electron app.
+
+** Pass in a number of seconds when running the app.
+``` npm start 10 ```
+will run Zen Mode for 10 seconds
+
+## Warning
+You ** Cannot ** get out of Zen mode so use with caution.
+
+
+
---
+++ renderer.js
@@ -0,0 +1,31 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+
+var remote = require('electron').remote;
+const {ipcRenderer} = require('electron');
+arguments = remote.getGlobal('sharedObject').prop1;
+let timer = (arguments[arguments.length - 1] * 1000)
+document.getElementById("url-list").innerHTML = (timer / 1000) + ' seconds'
+var currentTime = timer
+Number.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours + ':' + minutes + ':' + seconds;
+}
+setInterval(function(){
+    currentTime -= 1000
+    document.getElementById("url-list").innerHTML = (currentTime / 1000).toHHMMSS()
+
+},1000)
+setTimeout(function(){ipcRenderer.send('quit')}, timer)
